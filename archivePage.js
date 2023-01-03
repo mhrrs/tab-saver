d6f6c187ab32f08bc4652e3697b8959a05eccf43
harrismh <68102112+harrismh@users.noreply.github.com>
2023-01-03 15:29:54 -0700

    Update
    Project Update.

---
+++ archivePage.js
@@ -0,0 +1,13 @@
+//All of the following functions are for the archive page of the extension
+
+var currentPage = "archivePage.html"
+
+//------------- BACK BUTTON ----------------------
+document.getElementById("back-btn").addEventListener('click', function(e){
+    if(currentPage != "popup.html"){
+      console.log("Back button pressed.")
+      window.location = "popup.html";
+    }
+    else{console.log("Cannot go back.")};
+  });
+  
---
+++ popup.js
@@ -0,0 +1,58 @@
+let myLeads = []
+let windowArchive = {}
+let myUrls = ""
+let name_of_list = ""
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const archBtn = document.getElementById("archive-btn")
+var currentPage = "popup.html"
+
+
+///////////////////////////////////CONNECTING BUTTONS///////////////////////////////////////////////
+
+//############### SAVE BUTTON #################
+//submission info regarding Input button for "enter" key
+inputEl.addEventListener("keyup", function(event){
+  if(event.keyCode === 13){
+    inputBtn.click();
+
+    //set input value equal to the name of the window archive, the text inside the input button is then reset to ""
+    name_of_list = inputEl.value
+    inputEl.value = ""
+
+    //get all tabs open in window and insert into tabURLS array
+    chrome.tabs.query({}, function(tabs){
+        const tabURLS = tabs.map(tab => tab.url);
+        console.log(name_of_list)
+        console.log(tabURLS)
+
+        //turns the data into a dictionary where the archive name is the key and url array is the value
+        windowArchive = {name_of_list: tabURLS}
+        console.log(Object.entries(windowArchive))
+    
+        //adds urls from each tab into content.html
+        for(let i = 0; i < tabURLS.length; i++){
+        ulEl.innerHTML += `
+        <li>
+            ${"TAB: "}
+            <a target='_blank' href='${tabURLS[i]}'>
+            ${tabURLS[i]}
+            </a>
+        </li>
+        `
+        }
+    });
+  }
+})
+
+
+
+//------------- ARCHIVE HISTORY BUTTON -----------
+archBtn.addEventListener('click',function(e){
+  currentPage = 'archivePage.html';
+  window.location = currentPage;
+});
+
+////////////////////////// NON-BUTTON RELATED/////////////////////////////////////
+
